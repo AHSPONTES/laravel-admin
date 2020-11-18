162cf9df70cc18f7ece8529b3dbf2908a531dfe0
Andre Pontes <andrehspontes@gmail.com>
2020-11-18 07:22:45 -0300

    React and Laravel: Breaking a Monolith to Microservices

---
+++ app/User.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    protected $guarded = ['id'];
+
+    // protected $fillable = [
+    //     'first_name','last_name', 'email', 'password',
+    // ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+    ];
+}
